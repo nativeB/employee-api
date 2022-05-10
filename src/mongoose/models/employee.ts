10e1359cea392be8b98331440299049df912b273
quincy <qhutchison@infonaligy.com>
2022-05-10 12:39:17 +0000

    add employee model

---
+++ src/mongoose/models/employee.ts
@@ -0,0 +1,59 @@
+import * as mongoose from "mongoose"
+
+export interface EmployeeInterface {
+  firstName: string,
+  lastName: string,
+  dateOfBirth: Date,
+  jobTitle: string,
+  company: string,
+  country: string,
+}
+
+interface EmployeeModelInterface extends mongoose.Model<EmployeeDocumentInterface> {
+}
+export interface EmployeeDocumentInterface extends mongoose.Document {
+  firstName: string,
+  lastName: string,
+  dateOfBirth: Date,
+  jobTitle: string,
+  company: string,
+  country: string,
+}
+
+const EmployeeSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  }
+},
+{
+  timestamps: true
+})
+const Employee = mongoose.model<EmployeeDocumentInterface, EmployeeModelInterface>("Employee", EmployeeSchema)
+
+
+export { Employee }
+
+
+
+
